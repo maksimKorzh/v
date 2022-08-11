2d83b474eb5c121dbd4bdd4cbfdcf8bb1baa7e48
Code Monkey King <freesoft.for.people@gmail.com>
2022-08-12 00:27:55 +0300

    added enter key

--- v.c
+++ v.c
@@ -27,27 +27,38 @@ int main(int argc, char **argv) {
     addch(b[brw][bcl]); } clrtoeol(); addstr(brw < b.size() ? "\n" : "\n~"); }
     string stat = mod + " \"" + src + "\" " + to_string(r+1) + "/" + to_string(b.size());
     stat += b.size() ? " --" + to_string((int)((r+1)*100/b.size())) + "%-- " : "";
-    stat += "col " + to_string(c+1) + " : " + cnt;
+    stat += "col " + to_string(c+1) + " --x" + (cnt.length() ? cnt : "1") + "--";
     move(R, 0); for (int i = 0; i < stat.length(); i++) addch(stat[i]);
     clrtoeol(); curs_set(0); move(r - y, c - x); curs_set(1); refresh();
     int ch = -1; while (ch == -1) ch = getch(); string modes = "irR";
-    if (isdigit(ch) && ch != '0' && !(modes.find(mod[0]) != string::npos)) cnt += ch;
+    if (isdigit(ch) && !(modes.find(mod[0]) != string::npos)) cnt += ch;
     if (ch == ('[' & 0x1f)) { mod = 'n'; cnt = ""; continue; } int times = atoi(cnt.c_str());
-    for (int i = 0; i < (cnt.length() ? times : 1); i++) {
-      if (mod == "n") {
-        switch (ch) {
-          case 'i': mod == "i"; break;
-          case 'h': c ? c -= 1 : c; break;
-          case 'j': r < b.size() ? r += 1 : r; break;
-          case 'k': r ? r -= 1 : r; break;
-          case 'l': c < b[r].size()-1 ? c += 1 : c; break;
-        } int rwl = r < b.size() ? b.size() : 0; if (c > rwl -1) c = rwl ? rwl-1 : rwl; 
-      } else if (mod == "i") {
-      
+    if (mod == "n") {
+      if (ch == 'i') { mod = "i"; continue; }
+      if (ch == 'q') break;
+      else {
+        for (int i = 0; i < (cnt.length() ? times : 1); i++) {
+          switch (ch) {
+            case 'h': c ? c-- : c; break;
+            case 'j': r < b.size()-1 ? r++ : r; break;
+            case 'k': r ? r-- : r; break;
+            case 'l': c < b[r].size()-1 ? c++ : c; break;
+          }
+        } int rwl = r < b.size() ? b[r].size() : 0; if (c > rwl -1) c = rwl ? rwl-1 : rwl;
+        if (c == 0 || c == b[r].size()-1 || r == 0 || r == b.size()-1) cnt = "";
+      }
+    } else if (mod == "i") {
+      if (ch != (ch & 0x1f) && ch < 128) { b[r].insert(b[r].begin() + c, ch); c++; }
+      else if (ch == '\n') {
+        vector<int> right(b[r].size() - c);
+        copy(b[r].begin() + c, b[r].begin() + b[r].size(), right.begin());
+        vector<int> left(c); copy(b[r].begin(), b[r].begin() + c, left.begin());
+        b[r].clear(); b[r] = left; r++; c = 0; b.insert(b.begin() + r, right);
+        left.clear(); right.clear();
       }
     }
-    if (ch == KEY_RESIZE) { getmaxyx(stdscr, R, C); R -= 1; r = c = 0; refresh(); }
-    if (ch == 'q') break;
+    
+    if (ch == KEY_RESIZE) { getmaxyx(stdscr, R, C); R--; r = c = 0; refresh(); }
   }
   endwin();
   b.clear();
