54732bd4b7e4d470aeee21b8c02f35933e6cc68f
Code Monkey King <freesoft.for.people@gmail.com>
2022-08-12 21:49:29 +0300

    implemented delete

--- v.c
+++ v.c
@@ -49,8 +49,10 @@ int main(int argc, char **argv) {
       else if (ch == 'y' || ch == 'd') {
         bf.clear(); for (int i = 0; i < (cnt.length() ? times : 1); i++) {
         if (r+i < b.size()) { vector<int> row; for (int j = 0; j < b[r+i].size(); j++) {
-        row.insert(row.begin() + j, b[r+i][j]); } bf.push_back(row);
-        if (ch == 'd') b.erase(b.begin() + r + i); }} cnt = "";
+        row.insert(row.begin() + j, b[r+i][j]); } bf.push_back(row); }}
+        if (ch == 'd') for (int i = 0; i < (cnt.length() ? times : 1); i++) {
+        if (b.size() > 1 && r < b.size()) b.erase(b.begin() + r); if (r == b.size()) r--;} cnt = "";
+        msg = (ch == 'y' ? "Yank " : "Delete ") + to_string(bf.size()) + " line(s)";
       } else if (ch == ' ' || ch == 127) {
         for (int i = 0; i < (cnt.length() ? times : 1); i++) {
           if (r+i < b.size()) { if (ch == ' ' && c < b[r+i].size()) b[r+i].insert(b[r+i].begin() + c, 32);
